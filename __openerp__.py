908acd16729c9effab856c5bc4c2744f82027dc5
Noblex1991 <Noblex1991@users.noreply.github.com>
2017-03-22 12:57:28 +0100

    Firts Commi
    A custom creation of a theme for Odoo V8 used for www.gustop.net

---
+++ __openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Theme Precious',
+    'description': 'Excelsior Theme',
+    'author': "Alessandro D'Armiento",
+    'category': 'Theme',
+    'version': '1.0',
+    'depends':[
+        'base',
+        'website',
+        'website_less',
+    ],
+    'data': [
+        'views/assets.xml',
+        'views/layout.xml',
+    ],
+    'demo': [
+    ],
+    'tests': [
+    ],
+}
